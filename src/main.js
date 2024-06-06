7ef4c1bed3bd13154dc94837e7e14a612c3cf26c
Borna <Borna1103@msn.com>
2024-06-05 22:58:07 -0700

    Set Up

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/**
+ * @file JavaScript Code for front page
+ * @author Borna Hemmaty
+ * @author Jorge Ramos
+ */
+
+const backbutton = document.getElementById('test');
+
+
+
+backbutton.addEventListener('click', response)
+
+ function response(){
+    alert('123');
+}
